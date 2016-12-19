983166da4f069a7c90580e65239b4b7f0cb4bafa
Dave Stearns <dlsinfo@uw.edu>
2016-12-19 13:39:02 -0800

    overall framework and getting setup tutorial

---
+++ gulpfile.js
@@ -0,0 +1,169 @@
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
+const showdown = require('showdown');
+const through = require('through2');
+const del = require('del');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const $ = require('gulp-load-plugins')();
+
+const DIST_DIR = './docs';
+
+//adds class="table" to tables so that Bootstrap
+//will format them nicely
+var showdownTableExtension = function() {
+    return [{
+        type: 'output',
+        regex: '<table>',
+        replace: '<table class="table">'
+    }];
+};
+
+var mdConverter = new showdown.Converter({
+    omitExtraWLInCodeBlocks: true,
+    prefixHeaderId: "sec-",
+    parseImgDimensions: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tables: true,
+    extensions: [showdownTableExtension]
+});
+var htmlminOpts = {
+    collapseWhitespace: true,
+    conservativeCollapse: true    
+};
+
+//template for all tutorials
+var template = handlebars.compile(fs.readFileSync('./src/template.html', {encoding: 'utf8'}));
+
+//mergeHTML merges tutorial file with the template. 
+//Title and subtitle come from meta.json in the same dir as the template
+function mergeHTML(file, enc, cb) {
+    var meta;
+    try {
+        meta = require(path.join(path.dirname(file.path), 'meta.json'));
+    } catch(err) {
+        throw new $.util.PluginError('MergeHTML', 'Missing or invalid meta.json for file ' 
+            + file.path + ': ' + err);
+    }
+
+    //tutorial directory name == tutorial name
+    var tutorialName = path.basename(path.resolve(file.path, '..'));
+
+    meta.headerClass = meta.headerClass || tutorialName + '-header';
+
+    var merged = template({
+        meta: meta,
+        content: file.contents.toString('utf8')
+    });
+    file.contents = Buffer.from(merged, 'utf8');
+    cb(null, file);
+}
+
+//convertMarkdown converts a markdown tutorial to HTML
+function convertMarkdown(file, enc, cb) {
+    var html = mdConverter.makeHtml(file.contents.toString('utf8'));
+    file.contents = Buffer.from(html, 'utf8');
+    cb(null, file);
+}
+
+//mergeIndex merges the root index.html to produce
+//a table of contents page
+function mergeIndex(file, enc, cb) {
+    var indexTemplate = handlebars.compile(file.contents.toString(enc));
+    var merged = indexTemplate({contents: gatherContents()});
+    file.contents = Buffer.from(merged, 'utf8');
+    cb(null, file)
+}
+
+//gatherContents loads ./src/contents.json to get the
+//ordered list of tutorials for the contents page
+function gatherContents() {
+    var contents = require('./src/contents.json');
+    return contents.map(dir => {
+        try {
+            var meta = require('./src/' + path.join(dir, 'meta.json'));
+            meta.url = dir;
+            return meta;
+        } catch(err) {
+            throw new $.util.PluginError('Gather Contents', 
+                'Missing or invalid meta.json for file ' 
+                + ': ' + err);
+        }
+    });
+}
+
+gulp.task('merge-md', () => {
+    return gulp.src('./src/*/*.md')
+        .pipe(through.obj(convertMarkdown))
+        .pipe(through.obj(mergeHTML))
+        .pipe($.rename({extname: '.html'}))
+        .pipe(gulp.dest(DIST_DIR))
+});
+
+gulp.task('merge-html', () => {
+    return gulp.src('./src/*/*.html')
+        .pipe(through.obj(mergeHTML))
+        .pipe($.htmlmin(htmlminOpts))
+        .pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('images', () => {
+    return gulp.src('./src/*/img/*')
+        .pipe($.cache($.imagemin()))
+        .pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('global-css', () => {
+    return gulp.src(['./src/css/**', './src/*/css/*'])
+        .pipe($.concatCss('styles.css'))
+        .pipe($.autoprefixer('last 2 version'))
+        .pipe($.cssnano())
+        .pipe(gulp.dest(DIST_DIR + '/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('global-img', () => {
+    return gulp.src('./src/img/**')
+        .pipe($.cache($.imagemin()))
+        .pipe(gulp.dest(DIST_DIR + '/img'));
+});
+
+gulp.task('global-lib', () => {
+    return gulp.src('./src/lib/**')
+        .pipe(gulp.dest(DIST_DIR + '/lib'));
+});
+
+gulp.task('index', () => {
+    return gulp.src('./src/index.html')
+        .pipe(through.obj(mergeIndex))
+        .pipe($.htmlmin(htmlminOpts))
+        .pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('clean', () => {
+    return del(DIST_DIR);
+});
+
+gulp.task('build', [
+    'merge-html', 
+    'merge-md', 
+    'images',
+    'global-css', 
+    'global-img', 
+    'global-lib', 
+    'index'
+]);
+
+gulp.task('watch', ['build'], () => {
+    browserSync.init({
+        server: DIST_DIR,
+        files: DIST_DIR
+    });
+    gulp.watch(['./src/css/**', './src/*/css/*'], ['global-css']);
+    gulp.watch('./src/*/*.md', ['merge-md']);
+    gulp.watch('./src/*/*.html', ['merge-html']);
+});
+
+gulp.task('default', ['build']);
---
+++ src/getting-setup/index.md
@@ -0,0 +1,24 @@
+Throughout this course you will be building web sites from scratch and managing your files in a version control system, so you need to install a few tools on your computer and create accounts on a few services. All of these tools are free and available on all major platforms (Windows, OS X, and Linux).
+
+## GitHub Account
+
+First, [sign up for a free GitHub account](https://github.com/join) if you don't already have one. GitHub is a web service that allows you to get starter code from us, backup your code so that it doesn't get lost, share it with us when you have questions, and submit your final code to us when you're done with a challenge. It's kind of like Dropbox or Google Drive, only much more powerful. You will learn all about GitHub during the week on Managing Your Files.
+
+## GitHub Desktop or Git Command-Line Tools
+
+After you create your GitHub account, install the [GitHub Desktop](https://desktop.github.com/) application. This tool will help you manage your development files on your local machine, and synchronize them with GitHub.
+
+The GitHub Desktop application provides a simple, friendly, and easy-to-use graphical user interface over the more complex but far more powerful git command-line tools. If you are comfortable using the command line on your computer, you may want to install the [git command line tools](https://git-scm.com/downloads) in addition to the GitHub Desktop tool. You may use either (or both) for this course.
+
+## A Web-Optimized Code Editor
+
+The files for a web site or application are written in plain text, so you can use any text editor to write them, but there are some editors that are optimized for web development, and they make your life *much* easier.
+
+The editor I would highly recommend for this course is [Visual Studio Code](https://code.visualstudio.com/). This is a free, cross-platform, and feature-rich code editor from Microsoft (yes, a free tool from Microsoft!). It has a ton of useful features built-in, and a growing [library of extensions](https://marketplace.visualstudio.com/VSCode). It also has fantastic support for JavaScript.
+
+The other popular editor for web development is [Sublime Text](http://www.sublimetext.com/). You can download it for free, but it requires a $70 license to use it long-term. Sublime is a bit snappier than VS Code, but it doesn't come with as many features built-in. Instead, you need to install the [package control](https://packagecontrol.io/installation) extension, and then use it to install other extensions you might want. If you choose to use Sublime, I would highly recommend installing package control, and then use it to install the [emmet extension](https://github.com/sergeche/emmet-sublime#readme) for Sublime. This will give you the same powerful code completions that you get by default with VS Code.
+
+## A Browser with Developer Tools
+
+Although any browser will display the web sites we create during this course, you'll want to use a web browser that has a built-in set of developer tools so that you can better diagnose problems with your code. I would highly recommend using either [Google Chrome](https://www.google.com/chrome/) or [Mozilla Firefox](https://www.mozilla.org/en-US/firefox/new/).
+
